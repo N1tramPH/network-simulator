6df23ceca3add992ec5ad8b01b06777314cddc03
N1tramPH <m.phan231@gmail.com>
2024-06-10 16:07:32 +0200

    Refactor: history component

--- src/components/transmission/historySection/HistorySection.vue
+++ src/components/transmission/historySection/HistorySection.vue
@@ -1,42 +1,58 @@
 <script setup>
-import { ref } from "vue";
 import { storeToRefs } from "pinia";
 
 import { useTransmitStore } from "../../../stores/TransmitStore";
 import { useSimulationStore } from "../../../stores/SimulationStore";
 import Collapsable from "../../Collapsable.vue";
+import Packet from "../../board/moveables/Packet/Packet.vue";
 
-let visualized = ref(null);
+/**
+ * Currently visualized history.
+ */
 let currentHistory = null;
-const history = useTransmitStore().history;
 
-const store = useSimulationStore();
-const { activeLabel } = storeToRefs(store);
+const histories = useTransmitStore().history;
+const simulation = useSimulationStore();
 
-const isVisualized = (packet) => {
-  return visualized.value && packet.id === visualized.value.id;
+const { activeLabel } = storeToRefs(simulation);
+
+/**
+ * Checks if given history is being visualized.
+ * Used for prevention of animation recomputation.
+ * @param {Packet} packet
+ */
+const isHistoryVisualized = (history) => {
+  return currentHistory && history.id === currentHistory.id;
+};
+
+const isPacketVisualized = (packet) => {
+  return activeLabel.value === packet.label;
 };
 
 /**
- * Visualizes a transmission given a by a root packet
- * also moving within the visualization timeline specifying a label
- * @param {Packet} packet A root packet representing a transmission
+ * Visualizes a transmission from given a history.
+ * Optionally moving to the given position in timeline by specifying a label
+ * @param {Object} history A history that's to be visualized
  * @param {String} label A label in a given transmission timeline
  */
-async function visualize(packet, label = "") {
+async function visualize(history, label = "") {
+  const packet = history.packet;
+
   try {
     // There is no visualization initialized
-    if (!isVisualized(packet)) {
-      visualized.value = packet;
-      await store.animatePacket(packet);
+    if (!isHistoryVisualized(history)) {
+      currentHistory = history;
+      await simulation.animatePacket(packet);
     }
 
-    if (label) store.seek(label);
-    store.play();
+    // If the label within animation is specified
+    if (label) simulation.seek(label);
+    simulation.play();
   } catch (e) {
     const confirmation = window.confirm(
       "This animations seems to be corrupted. Would you like to remove it?"
     );
+
     if (confirmation && currentHistory) {
       useTransmitStore().removeHistory(currentHistory.id);
     }
@@ -46,14 +62,17 @@ async function visualize(packet, label = "") {
 </script>
 
 <template>
-  <ul v-if="history.length" class="rounded-lg max-w-xs w-full p-0">
-    <div v-for="(t, idx) in history" :key="idx">
+  <ul v-if="histories.length" class="rounded-lg max-w-xs w-full p-0">
+    <div v-for="(history, idx) in histories" :key="idx">
       <Collapsable>
         <template v-slot:header>
           <header>
-            <span class="label text-sm inline"> {{ t.label }}&nbsp; </span>
+            <span class="label text-sm inline">
+              {{ history.label }}&nbsp;
+            </span>
+
             <span class="text-xs">
-              ({{ t.client.name }} &#8594; {{ t.server.name }})
+              ({{ history.client.name }} &#8594; {{ history.server.name }})
             </span>
           </header>
         </template>
@@ -61,17 +80,14 @@ async function visualize(packet, label = "") {
         <template v-slot:body>
           <ul class="p-0 ml-1 w-full">
             <li
-              v-for="packet in t.packet.flatten()"
-              :key="packet.id"
-              @click="
-                visualize(t.packet, packet.label);
-                currentHistory = t;
-              "
+              v-for="packet in history.packet.flatten()"
               class="hover:opacity-75 cursor-pointer py-0.5 pl-2 my-0.5 text-[0.77rem]"
               :class="{
                 'font-semibold text-cyan-600 dark:text-amber-400':
-                  packet.label === activeLabel,
+                  isPacketVisualized(packet),
               }"
+              :key="packet.id"
+              @click="visualize(history, packet.label)"
             >
               {{ packet.title }} &nbsp; ({{
                 packet.startPoint ? packet.startPoint.name : ""
